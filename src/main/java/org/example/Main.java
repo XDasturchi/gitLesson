b4e213469630839f2be6c6fb1433c56563842325
Xojiakbar Kamolov <xdasturchi777@gmail.com>
2023-07-18 10:29:34 +0500

    Soutni 2 ta qildim

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,24 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StringBuilder builder = new StringBuilder();
-
-        for (int i = 0; i < 10; i++){
-            Faker faker = new Faker();
-
-            String  name = faker.name().firstName();
-
-            System.out.println(name);
-
-            if(builder.toString().contains(name)){
-                System.out.println("Oldin Kelgan: " + name);
-            }
-
-            builder.append(name);
-
-        }
-
         System.out.println("Ishladi");
+
+        System.out.println("Assalomu Alaykum");
     }
 
 }
